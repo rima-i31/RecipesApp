e3798600a9ce67ee6e13b76d30177df6a976e5d5
rima-i31 <143452410+rima-i31@users.noreply.github.com>
2024-08-09 23:10:30 +0300

    updatedSearching

---
+++ intershipApp/Controllers/NoRecipeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NoRecipeTableViewCell.swift
+//  intershipApp
+//
+//  Created by Rima Mihova on 09.08.2024.
+//
+
+import UIKit
+
+class NoRecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
